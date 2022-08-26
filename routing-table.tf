da3d99f607919f460d199dd5c0b2f1c8b43d95b9
Baldwinma <baldwinapiafor@gmail.com>
2022-08-26 11:04:05 -0400

    editing routing-table file

--- routing-table.tf
+++ routing-table.tf
@@ -2,15 +2,10 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/route_table
 
 resource "aws_route_table" "public" {
-  # The VPC ID.
-  vpc_id = aws_vpc.main.id
-
+  vpc_id = aws_vpc.eks_vpc.id
   route {
-    # The CIDR block of the route.
-    cidr_block = "0.0.0.0/0"
-
-    # Identifier of a VPC internet gateway or a virtual private gateway.
-    gateway_id = aws_internet_gateway.main.id
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = aws_internet_gateway.eks_vpc.id
   }
 
   # A map of tags to assign to the resource.
@@ -20,14 +15,9 @@ resource "aws_route_table" "public" {
 }
 
 resource "aws_route_table" "private1" {
-  # The VPC ID.
-  vpc_id = aws_vpc.main.id
-
+  vpc_id = aws_vpc.eks_vpc.id
   route {
-    # The CIDR block of the route.
     cidr_block = "0.0.0.0/0"
-
-    # Identifier of a VPC NAT gateway.
     nat_gateway_id = aws_nat_gateway.gw1.id
   }
 
@@ -38,14 +28,9 @@ resource "aws_route_table" "private1" {
 }
 
 resource "aws_route_table" "private2" {
-  # The VPC ID.
-  vpc_id = aws_vpc.main.id
-
+  vpc_id = aws_vpc.eks_vpc.id
   route {
-    # The CIDR block of the route.
     cidr_block = "0.0.0.0/0"
-
-    # Identifier of a VPC NAT gateway.
     nat_gateway_id = aws_nat_gateway.gw2.id
   }
 
