90250996a3045040973c4f33ec61f348cd241d03
Baldwinma <baldwinapiafor@gmail.com>
2022-08-26 10:51:05 -0400

    adding subnets file

---
+++ subnets.tf
@@ -0,0 +1,58 @@
+# Resource: aws_subnet
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/subnet
+
+resource "aws_subnet" "public_1" {
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = "192.168.0.0/18"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                        = "public-us-east-1a"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = "192.168.64.0/18"
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                        = "public-us-east-1b"
+    "kubernetes.io/cluster/eks" = "shared"
+    "kubernetes.io/role/elb"    = 1
+  }
+}
+
+resource "aws_subnet" "private_1" {
+
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = "192.168.128.0/18"
+  availability_zone = "us-east-1a"
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                              = "private-us-east-1a"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+resource "aws_subnet" "private_2" {
+
+  vpc_id = aws_vpc.eks_vpc.id
+  cidr_block = "192.168.192.0/18"
+  availability_zone = "us-east-1b"
+
+  # A map of tags to assign to the resource.
+  tags = {
+    Name                              = "private-us-east-1b"
+    "kubernetes.io/cluster/eks"       = "shared"
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
